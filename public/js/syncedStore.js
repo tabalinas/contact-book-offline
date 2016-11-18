00df98496e77fe3c8c23b8e0570c0632490718a0
Artem Tabalin <tabalinas@gmail.com>
2016-11-18 01:13:47 +0100

    Store: Finish sync store implementation

--- public/js/syncedStore.js
+++ public/js/syncedStore.js
@@ -1,14 +1,31 @@
 (function(Store) {
 
-    function SyncedStore(name, serverUrl) {
-        this.serverUrl = serverUrl;
-        this.reqQueue = [];
+    var REQUEST_RETRY_DELAY = 500;
+
+    function SyncedStore(name, endpointUrl) {
+        this.endpointUrl = endpointUrl;
+
+        this.attachOnlineHandler();
 
         Store.call(this, name);
-        this.attachOnlineHandler();
+
+        this.loadRequestQueue(name);
+        this.processQueue();
     }
 
     SyncedStore.prototype = Object.create(Store.prototype);
+
+    SyncedStore.prototype.loadRequestQueue = function(storeName) {
+        this.requestQueueStorageName = "__requests_" + storeName;
+
+        var storedQueue = localStorage.getItem(this.requestQueueStorageName) || "[]";
+
+        this.requestQueue = JSON.parse(storedQueue);
+    };
+
+    SyncedStore.prototype.saveRequestQueue = function() {
+        localStorage.setItem(this.requestQueueStorageName, JSON.stringify(this.requestQueue));
+    };
 
     SyncedStore.prototype.attachOnlineHandler = function() {
         window.addEventListener("online", function() {
@@ -22,7 +39,10 @@
         return promise.then(function(res) {
             return this.get(res.id);
         }.bind(this)).then(function(addedItem) {
-            this.enqueueRequest("POST", addedItem);
+            this.enqueueRequest({
+                method: "POST",
+                data: addedItem
+            });
         }.bind(this));
     };
 
@@ -32,7 +52,11 @@
         return promise.then(function(res) {
             return this.get(res.id);
         }.bind(this)).then(function(updatedItem) {
-            this.enqueueRequest("PUT", updatedItem);
+            this.enqueueRequest({
+                method: "PUT",
+                id: updatedItem._id,
+                data: updatedItem
+            });
         }.bind(this));
     };
 
@@ -40,36 +64,63 @@
         var promise = Store.prototype.remove.call(this, id);
 
         return promise.then(function() {
-            this.enqueueRequest("DELETE", id);
+            this.enqueueRequest({
+                method: "DELETE",
+                id: id
+            });
         }.bind(this));
     };
 
-    SyncedStore.prototype.enqueueRequest = function(type, data) {
-        this.reqQueue.push({
-            method: type,
-            data: data
-        });
+    SyncedStore.prototype.enqueueRequest = function(req) {
+        this.requestQueue.push(req);
+        this.saveRequestQueue();
         this.processQueue();
     };
 
     SyncedStore.prototype.processQueue = function() {
-        if(!navigator.onLine)
+        if(!navigator.onLine || !this.requestQueue.length)
             return;
 
-        while(this.reqQueue.length) {
-            var req = this.reqQueue[0];
+        var req = this.requestQueue[0];
 
-            this.sendRequest(req).then(function() {
-                this.reqQueue.unshift();
+        this.sendRequest(req,
+            function() {
+                this.requestQueue.shift();
+                this.saveRequestQueue();
+
                 this.processQueue();
+            }.bind(this), function(e) {
+                console.error(e);
+
+                setTimeout(function() {
+                    this.processQueue();
+                }.bind(this), REQUEST_RETRY_DELAY);
             }.bind(this));
-        }
     };
 
-    SyncedStore.prototype.sendRequest = function(req) {
-        // TODO: implement sending request
+    SyncedStore.prototype.sendRequest = function(req, done, fail) {
+        try {
+            var url = this.endpointUrl + (req.id ? "/" + req.id : "");
+
+            var xhr = new XMLHttpRequest();
+            xhr.open(req.method, url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+
+            xhr.onload = function() {
+                if(xhr.status >= 200 && xhr.status < 300) {
+                    done(xhr.responseText);
+                } else {
+                    fail(xhr.statusText);
+                }
+            };
+
+            xhr.send(req.data ? JSON.stringify(req.data) : null);
+
+        } catch(e) {
+            fail(JSON.stringify(e));
+        }
     };
 
     window.SyncedStore = SyncedStore;
 
-}(PouchDB));
+}(Store));
