29b070e4262db850b046066802b42bb315767985
Artem Tabalin <tabalinas@gmail.com>
2016-11-18 09:53:10 +0100

    Offline: Add service worker support

---
+++ public/js/register-service-worker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js', {
+        scope: '/'
+    }).then(function() {
+        console.info("service worker registered");
+    }).catch(function(e) {
+        console.error(e, "service worker registration failed");
+    });
+}
---
+++ public/service-worker.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'contact-book-v1';
+
+var resourcesToCache = [
+    '/',
+    '/css/style.css',
+    '/js/ext/pouchdb.min.js',
+    '/js/register-service-worker.js',
+    '/js/store.js',
+    '/js/syncedStore.js',
+    '/js/app.js'
+];
+
+self.addEventListener('install', function(event) {
+    console.info("installing service worker");
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.info("cache opened");
+
+                return cache.addAll(resourcesToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    console.info("service worker activated");
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                console.log("request:", event.request);
+
+                if (response) {
+                    console.info("cache hit");
+                    return response;
+                }
+
+                console.info("fetching");
+                return fetch(event.request);
+            })
+    );
+});
