df9641a72bd6b748a7e6b2026793e62542a8f7fb
Artem Tabalin <tabalinas@gmail.com>
2016-11-30 10:08:30 +0100

    App: ES5 => ES6 syntax

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,27 @@
-(function() {
 
-    var CONTACT_ID_ATTR_NAME = "data-contractid";
-    var CONTACT_REMOVE_CONFIRM = "Are you sure?";
-    var NO_CONTACTS_TEXT = "No contacts";
+var CONTACT_ID_ATTR_NAME = "data-contractid";
+var CONTACT_REMOVE_CONFIRM = "Are you sure?";
+var NO_CONTACTS_TEXT = "No contacts";
 
-    function ContactBook(storeClass, remote) {
-        this.store = new storeClass("contacts", remote, function() {
+class ContactBook {
+
+    constructor(storeClass, remote) {
+        this.store = new storeClass("contacts", remote, () => {
             this.refresh();
-        }.bind(this));
+        });
 
         this.init();
         this.refresh();
         this.toggleContactFormEditing(false);
     }
 
-    ContactBook.prototype.init = function() {
+    init() {
         this.initElements();
         this.initItemTemplate();
         this.attachHandlers();
-    };
+    }
 
-    ContactBook.prototype.initElements = function() {
+    initElements() {
         this.contactList = document.getElementById("contactList");
 
         this.contactDetailsForm = document.getElementById("contactDetails");
@@ -34,142 +35,145 @@
         this.removeContactButton = document.getElementById("removeContact");
         this.saveContactButton = document.getElementById("saveContact");
         this.cancelEditButton = document.getElementById("cancelEdit");
-    };
+    }
 
-    ContactBook.prototype.initItemTemplate = function() {
+    initItemTemplate() {
         var contactListItem = this.contactList.querySelector("li");
         this.contactList.removeChild(contactListItem);
         this._contactTemplate = contactListItem;
-    };
+    }
 
-    ContactBook.prototype.attachHandlers = function() {
-        this.contactDetailsForm.addEventListener("submit", function(event) {
+    attachHandlers() {
+        this.contactDetailsForm.addEventListener("submit", event => {
             event.preventDefault();
-        }.bind(this));
+        });
 
-        this.addContactButton.addEventListener("click", this.addContact.bind(this));
-        this.editContactButton.addEventListener("click", this.editContact.bind(this));
-        this.removeContactButton.addEventListener("click", this.removeContact.bind(this));
-        this.saveContactButton.addEventListener("click", this.saveContact.bind(this));
-        this.cancelEditButton.addEventListener("click", this.cancelEdit.bind(this));
-    };
+        this.addContactButton.addEventListener("click", () => { this.addContact() });
+        this.editContactButton.addEventListener("click", () => { this.editContact() });
+        this.removeContactButton.addEventListener("click", () => { this.removeContact() });
+        this.saveContactButton.addEventListener("click", () => { this.saveContact() });
+        this.cancelEditButton.addEventListener("click", () => { this.cancelEdit() });
+    }
 
-    ContactBook.prototype.refresh = function() {
-        this.renderContactList();
-    };
+    refresh() {
+        this.store.getAll().then(contacts => {
+            this.sortContacts(contacts);
+            this.renderContactList(contacts);
+        });
+    }
 
-    ContactBook.prototype.renderContactList = function() {
-        this.store.getAll().then(function(contacts) {
-            contacts.sort(function(contact1, contact2) {
-                 return (contact1.firstName + contact1.lastName).localeCompare(contact2.firstName + contact2.firstName);
-            });
+    sortContacts(contacts) {
+        contacts.sort((contact1, contact2) => {
+            return (contact1.firstName + contact1.lastName).localeCompare(contact2.firstName + contact2.firstName);
+        });
+    }
 
-            this.contactList.innerHTML = "";
-            this.contactList.appendChild(this.createContactList(contacts));
-        }.bind(this));
-    };
+    renderContactList(contacts) {
+        this.contactList.innerHTML = "";
+        this.contactList.appendChild(this.createContactList(contacts));
+    }
 
-    ContactBook.prototype.createContactList = function(contacts) {
+    createContactList(contacts) {
         if(!contacts.length)
             return this.createNoDataItem();
 
         var result = document.createDocumentFragment();
 
-        contacts.forEach(function(contact) {
+        contacts.forEach(contact => {
             result.appendChild(this.createContact(contact))
-        }.bind(this));
+        });
 
         return result;
-    };
+    }
 
-    ContactBook.prototype.createNoDataItem = function() {
+    createNoDataItem() {
         var result = document.createElement("li");
         result.className = "contact-list-empty";
         result.textContent = NO_CONTACTS_TEXT;
         return result;
-    };
+    }
 
-    ContactBook.prototype.createContact = function(contact) {
+    createContact(contact) {
         var result = this._contactTemplate.cloneNode(true);
         result.setAttribute(CONTACT_ID_ATTR_NAME, contact._id);
         result.querySelector(".contact-name").innerText = contact.firstName + " " + contact.lastName;
         result.querySelector(".contact-phone").innerText = contact.phone;
-        result.addEventListener("click", this.showContact.bind(this));
+        result.addEventListener("click", event => { this.showContact(event) });
         return result;
-    };
+    }
 
-    ContactBook.prototype.showContact = function(event) {
+    showContact(event) {
         var contactId = event.currentTarget.getAttribute(CONTACT_ID_ATTR_NAME);
 
-        this.store.get(contactId).then(function(contact) {
+        this.store.get(contactId).then(contact => {
             this.setContactDetails(contact);
             this.toggleContactFormEditing(false);
-        }.bind(this))
-    };
+        })
+    }
 
-    ContactBook.prototype.addContact = function() {
+    addContact() {
         this.setContactDetails({ firstName: "Name" });
         this.toggleContactFormEditing(true);
-    };
+    }
 
-    ContactBook.prototype.editContact = function() {
+    editContact() {
         var contactId = this.getContactId();
 
-        this.store.get(this.getContactId()).then(function(contact) {
+        this.store.get(this.getContactId()).then(contact => {
             this.setContactDetails(contact);
             this.toggleContactFormEditing(true);
-        }.bind(this));
-    };
+        });
+    }
 
-    ContactBook.prototype.saveContact = function() {
+    saveContact() {
         var contact = this.getContactDetails();
 
-        this.store.save(contact).then(function() {
+        this.store.save(contact).then(() => {
             this.setContactDetails({});
             this.toggleContactFormEditing(false);
             this.refresh();
-        }.bind(this));
-    };
+        });
+    }
 
-    ContactBook.prototype.removeContact = function() {
+    removeContact() {
         if(!window.confirm(CONTACT_REMOVE_CONFIRM))
             return;
 
         var contactId = this.getContactId();
 
-        this.store.remove(contactId).then(function() {
+        this.store.remove(contactId).then(() => {
             this.setContactDetails({});
             this.toggleContactFormEditing(false);
             this.refresh();
-        }.bind(this));
-    };
+        });
+    }
 
-    ContactBook.prototype.cancelEdit = function() {
+    cancelEdit() {
         this.setContactDetails({});
         this.toggleContactFormEditing(false);
-    };
+    }
 
-    ContactBook.prototype.getContactDetails = function() {
+    getContactDetails() {
         return {
             _id: this.getContactId(),
             firstName: this.firstNameField.value,
             lastName: this.lastNameField.value,
             phone: this.phoneField.value
         };
-    };
+    }
 
-    ContactBook.prototype.getContactId = function() {
+    getContactId() {
         return this.contactIdField.value;
-    };
+    }
 
-    ContactBook.prototype.setContactDetails = function(contactDetails) {
+    setContactDetails(contactDetails) {
         this.contactIdField.value = contactDetails._id || "";
         this.firstNameField.value = contactDetails.firstName || "";
         this.lastNameField.value = contactDetails.lastName || "";
         this.phoneField.value = contactDetails.phone || "";
-    };
+    }
 
-    ContactBook.prototype.toggleContactFormEditing = function(isEditing) {
+    toggleContactFormEditing(isEditing) {
         var isContactSelected = !this.getContactId();
 
         this.toggleFade(this.contactDetailsForm, !isEditing && isContactSelected);
@@ -187,24 +191,23 @@
 
         this.firstNameField.focus();
         this.firstNameField.setSelectionRange(0, this.firstNameField.value.length);
-    };
+    }
 
-    ContactBook.prototype.toggleElement = function(element, isShown) {
+    toggleElement(element, isShown) {
         element.style.display = isShown ? "block" : "none";
-    };
+    }
 
-    ContactBook.prototype.toggleFade = function(element, isFade) {
+    toggleFade(element, isFade) {
         element.style.opacity = isFade ? .5 : 1;
-    };
+    }
 
-    ContactBook.prototype.toggleDisabled = function(element, isDisabled) {
+    toggleDisabled(element, isDisabled) {
         if(isDisabled) {
             element.setAttribute("disabled", "");
         } else {
             element.removeAttribute("disabled");
         }
-    };
+    }
+}
 
-    window.ContactBook = ContactBook;
-
-}());
+window.ContactBook = ContactBook;
--- public/js/store.js
+++ public/js/store.js
@@ -1,62 +1,54 @@
-(function(PouchDB) {
 
-    function Store(name, remote, onChange) {
+class Store {
+
+    constructor(name, remote, onChange) {
         this.db = new PouchDB(name);
 
-        PouchDB.sync(name, remote + '/' + name, {
+        PouchDB.sync(name, `${remote}/${name}`, {
             live: true,
             retry: true
-        }).on('change', function (info) {
+        }).on('change', info => {
             onChange(info);
         });
     }
 
-    Store.prototype.getAll = function() {
+    getAll() {
         return this.db.allDocs({ include_docs: true })
-            .then(function(db) {
-                return db.rows.map(function(row) {
+            .then(db => {
+                return db.rows.map(row => {
                     return row.doc;
                 });
             });
-    };
+    }
 
-    Store.prototype.get = function(id) {
+    get(id) {
         return this.db.get(id);
-    };
+    }
 
-    Store.prototype.save = function(item) {
+    save(item) {
         return item._id
             ? this.update(item)
             : this.add(item);
-    };
+    }
 
-    Store.prototype.add = function(item) {
+    add(item) {
         return this.db.post(item);
-    };
+    }
 
-    Store.prototype.update = function(item) {
+    update(item) {
         return this.db.get(item._id)
-            .then(function(updatingItem) {
-                this.extend(updatingItem, item);
+            .then(updatingItem => {
+                Object.assign(updatingItem, item);
                 return this.db.put(updatingItem);
-            }.bind(this));
-    };
+            });
+    }
 
-    Store.prototype.remove = function(id) {
+    remove(id) {
         return this.db.get(id)
-            .then(function(item) {
+            .then(item => {
                 return this.db.remove(item);
-            }.bind(this));
-    };
+            });
+    }
+}
 
-    Store.prototype.extend = function(target, source) {
-        for(var key in source) {
-            if(source.hasOwnProperty(key)) {
-                target[key] = source[key];
-            }
-        }
-    };
-
-    window.Store = Store;
-
-}(PouchDB));
+window.Store = Store;
